61607d697a35db837c36da85220b623d02db97c2
unknown <FA22-BCS-051@network.dcui>
2025-03-20 10:23:29 +0500

    third lab

--- LAB_03/loops.py
+++ LAB_03/loops.py
@@ -1,8 +1,20 @@
 array=[12,43,54]
 
-for i in array:
-    print(i)
-    for i in range (1,5):
-        print(i)
+# for i in array:
+#     print(i)
+#     for i in range (1,5):
+#         print(i)
+# for i in range(1,11):
+#         print(f"{i} and its square is {i**2}")
 
+# num=10
+# for i in range(1,num):
+#     fac=i*i+1
+#     print(f"{fac}")
 
+# i=0
+# while(i<10):
+#     print(f"{i}")
+#     i=i+2
+# for i in range(1,10,-2):
+#     print(f"{i}")
