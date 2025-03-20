a3c329560bc5025e6fbefbd32f81d63839990b7e
unknown <FA22-BCS-051@network.dcui>
2025-03-20 09:24:40 +0500

    second commit

--- Python Advance/LAB_01/distanceP.py
+++ Python Advance/LAB_01/distanceP.py
@@ -3,6 +3,6 @@ x2 = float(input("Enter point x2: "))
 y1 = float(input("Enter point y1: "))
 y2 = float(input("Enter point y2: "))
 
-distance = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5
+distance =round( ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5,0)
 
 print(f"The distance of the points is {distance}")
---
+++ Python Advance/LAB_03/loops.py
@@ -0,0 +1,8 @@
+array=[12,43,54]
+
+for i in array:
+    print(i)
+    for i in range (1,5):
+        print(i)
+
+
